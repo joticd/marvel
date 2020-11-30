f416b604e35e4ac844fc890d7b8b7b7709a5bd95
unknown <joticd@gmail.com>
2020-11-30 23:53:57 +0100

    fix: Add hash

---
+++ src/api/Keys.tsx
@@ -0,0 +1,1 @@
+export default  {PRIVATEKEY : '304f21ac8fb3db8db0ec69dd213191ea', PUBLICKEY : '9e9f6982b02e6a22a675eb771e9b98022a95b8b2'};
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,9 +1,30 @@
 import React, {useEffect, useState} from 'react';
+import marvel from '../api/api';
+import PRIVATEKEY from '../api/Keys';
+import PUBLICKEY from '../api/Keys';
 
 const SearchBar : React.FC = () =>{
     const [term, setTerm] = useState('');
     useEffect(()=>{
         console.log(term)
+
+        const search = async () =>{
+            const ts: Date = new Date();
+        //     const data = await marvel.get('https://gateway.marvel.com/v1/public/characters', {
+        //        params : {
+        //         apikey: '304f21ac8fb3db8db0ec69dd213191ea',
+        //         limit: 20,
+        //         name:'deadpool'
+        //        }
+        //    });
+
+           console.log(PRIVATEKEY, PUBLICKEY)
+        }
+
+        if(term!==""){
+            search();
+        }
+
     }, [term]);
     return (
         <div className='search-bar ui segment'>
